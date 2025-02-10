dbd072f8e031b60a2945fd57456cf3c14cbf918f
Your Name <you@example.com>
2025-02-10 11:24:32 +0900

    second commit

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Điều khiển nút Next/Prev
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  setInterval(() => {
+    plusSlides(1);
+  }, 3000);
+});
